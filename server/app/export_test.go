904d4967f8a062c8d2f57643d3c23844f33b8a5e
Catalin I. Tomai <catalin.tomai@mattermost.com>
2026-07-15 14:37:12 +0200

    remove empty app/export_test.go placeholder

--- server/app/export_test.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-package app
-
-// This file exposes private Service internals to package-level tests. Add *ForTest accessors here
-// as needed.
